dd3322b0fa250a51c001f74115ed7f3796572a43
Joseph Kingry <jkingry@shoplogix.com>
2013-06-07 16:55:30 -0400

    Finished package 1 question 5

--- package1/package1_test.go
+++ package1/package1_test.go
@@ -54,3 +54,15 @@ func Test_Question4_DetectXorCypher(t *testing.T) {
 		t.Errorf("DetectXorLine(%v) = '%v' want '%v'", in, x, out)
 	}
 }
+
+// 5. Detect single-character Xor
+
+func Test_Question5_RepeatingXor(t *testing.T) {
+	const key, in, out = "ICE", "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	x := RepeatXor([]byte(key), []byte(in))
+
+	if HexEncodeToString(x) != out {
+		t.Errorf("RepeatXor(%v, %v) = '%v' want '%v'", key, in, x, out)
+	}
+}
