da337e8b233bff11d90a291fad0dbd2f1d1cf412
Joseph Kingry <jkingry@gmail.com>
2013-09-19 23:10:43 -0400

    Question 10 done, except for test

--- package2/package2.go
+++ package2/package2.go
@@ -2,6 +2,7 @@ package package2
 
 import (
 	"crypto/aes"
+	"bitbucket.org/jkingry/matsano/package1"
 )
 
 // 9. Implement PKCS#7 padding
@@ -12,7 +13,7 @@ The value of each added byte is the number of bytes that are added,
 	i.e. N bytes, each of value N are added. The number of bytes added will depend on the block boundary to which the message needs to be extended.
 The padding will be one of:
  */
-func Pkcs7(blockSize int, in []byte) []byte {
+func Pkcs7_pad(blockSize int, in []byte) []byte {
 	inLength := len(in)
 	padding := blockSize - (inLength % blockSize)
 
@@ -22,11 +23,23 @@ func Pkcs7(blockSize int, in []byte) []byte {
 
 	result := make([]byte, inLength + padding)
 	copy(result, in)
+
 	for i := 0; i < padding; i++ {
 		result[inLength + i] = byte(padding)
 	}
 
 	return result
+}
+
+func Pkcs7_unpad(in []byte) []byte {
+	lastByte := int(in[len(in) - 1])
+	for i := 1; i <= lastByte; i++ {
+		p := len(in) - i
+		if p < 0 { return in }
+		if in[p] != byte(lastByte) { return in }
+	}
+
+	return in[:len(in) - lastByte]
 }
 
 /*
@@ -56,19 +69,55 @@ SUBMARINE" with an IV of all ASCII 0 (\x00\x00\x00 &c)
 
  */
 
-func EncryptAes(decrypted, key []byte) []byte {
+func AesCBCEncrypt(key, iv, decrypted []byte) []byte {
+	key = Pkcs7_pad(aes.BlockSize, key)
+	iv = Pkcs7_pad(aes.BlockSize, iv)
+
 	cipher, _ := aes.NewCipher(key)
 
+	decrypted = Pkcs7_pad(cipher.BlockSize(), decrypted)
+
 	encrypted := make([]byte, len(decrypted))
+
+	lastBlock := iv
+	for i := 0; i < len(encrypted); i += cipher.BlockSize() {
+		e := i + cipher.BlockSize()
+
+		dblock := package1.FixedXor(decrypted[i:e], lastBlock)
+		eblock := encrypted[i:e]
+
+		cipher.Encrypt(eblock, dblock)
+
+		lastBlock = eblock
+	}
+
+	return encrypted
+}
+
+func AesCBCDecrypt(key, iv, encrypted []byte) []byte {
+	key = Pkcs7_pad(aes.BlockSize, key)
+	iv = Pkcs7_pad(aes.BlockSize, iv)
+
+	cipher, _ := aes.NewCipher(key)
+
+	decrypted := make([]byte, len(encrypted))
+
+	lastBlock := iv
 
 	for i := 0; i < len(encrypted); i += cipher.BlockSize() {
 		e := i + cipher.BlockSize()
 
-		eblock := decrypted[i:e]
+		eblock := encrypted[i:e]
 		dblock := decrypted[i:e]
 
 		cipher.Decrypt(dblock, eblock)
+
+		for i := 0; i < len(lastBlock); i++ {
+			dblock[i] = dblock[i] ^ lastBlock[i]
+		}
+
+		lastBlock = eblock
 	}
 
-	return decrypted
+	return Pkcs7_unpad(decrypted)
 }
--- package2/package2_commandLine.go
+++ package2/package2_commandLine.go
@@ -2,6 +2,7 @@ package package2
 
 import (
 	"fmt"
+	"crypto/aes"
 	"bitbucket.org/jkingry/matsano/cmd"
 	"bitbucket.org/jkingry/matsano/encoding"
 )
@@ -10,11 +11,52 @@ var Commands *cmd.Command = cmd.NewCommand("p2", "Package 2 commands")
 
 func init() {
 	var blockSize int
-	pkcs7 := Commands.Add("pkcs7", "[input]")
-	pkcs7.Flags.IntVar(&blockSize, "blockSize", 20, "block size")
-	pkcs7.Command = func(args []string) {
+	pad := Commands.Add("pad", "[input]")
+	pad.Flags.IntVar(&blockSize, "blockSize", 20, "block size")
+	pad.Command = func(args []string) {
 		encoding.SetDefault(encoding.Ascii, encoding.Ascii, encoding.Ascii)
 		input := encoding.In.Decode(cmd.GetInput(args, 0))
-		fmt.Print(encoding.Out.Encode(Pkcs7(blockSize, input)))
+		fmt.Print(encoding.Out.Encode(Pkcs7_pad(blockSize, input)))
+	}
+
+	unpad := Commands.Add("unpad", "[input]")
+	unpad.Command = func(args []string) {
+		encoding.SetDefault(encoding.Ascii, encoding.Ascii, encoding.Ascii)
+		input := encoding.In.Decode(cmd.GetInput(args, 0))
+		fmt.Print(encoding.Out.Encode(Pkcs7_unpad(input)))
+	}
+
+	encrypt := Commands.Add("encrypt", "[key] [iv] [input]")
+	encrypt.Command = func(args []string) {
+		encoding.SetDefault(encoding.Ascii, encoding.Ascii, encoding.Base64)
+		key := encoding.Key.Decode(cmd.GetInput(args, 0))
+		iv :=  make([]byte, aes.BlockSize)
+		nextArg := 1
+
+		if len(args) == 3 {
+			iv = encoding.Key.Decode(cmd.GetInput(args, nextArg))
+			nextArg += 1
+		}
+
+		input := encoding.In.Decode(cmd.GetInput(args, nextArg))
+
+		fmt.Print(encoding.Out.Encode(AesCBCEncrypt(key, iv, input)))
+	}
+
+	decrypt := Commands.Add("decrypt", "[key] (iv) [input]")
+	decrypt.Command = func(args []string) {
+		encoding.SetDefault(encoding.Base64, encoding.Ascii, encoding.Ascii)
+		key := encoding.Key.Decode(cmd.GetInput(args, 0))
+	    iv :=  make([]byte, aes.BlockSize)
+		nextArg := 1
+
+		if len(args) == 3 {
+			iv = encoding.Key.Decode(cmd.GetInput(args, nextArg))
+			nextArg += 1
+		}
+
+		input := encoding.In.Decode(cmd.GetInput(args, nextArg))
+
+		fmt.Print(encoding.Out.Encode(AesCBCDecrypt(key, iv, input)))
 	}
 }
