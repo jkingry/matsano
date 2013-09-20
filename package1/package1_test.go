61cbd9b8d1c26d5fa82ef9128e55ca9650e37bde
Joseph Kingry <jkingry@shoplogix.com>
2013-09-20 10:39:05 -0400

    Fixed up package1 test cases from encoding move

--- package1/package1_test.go
+++ package1/package1_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 	"io/ioutil"
 	"os"
-	"strconv"
 	"strings"
+	"bitbucket.org/jkingry/matsano/encoding"
 )
 
 // 1. Convert hex to base64 and back.
@@ -13,7 +13,7 @@ import (
 func Test_Question1_HexToBase64(t *testing.T) {
 	const in, out = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-	if x := Base64EncodeToString(HexDecodeString(in)); x != out {
+	if x := encoding.Base64EncodeToString(encoding.HexDecodeString(in)); x != out {
 		t.Errorf("Base64EncodeToString(HexDecodeString(%v)) = %v, want %v", in, x, out)
 	}
 }
@@ -21,7 +21,7 @@ func Test_Question1_HexToBase64(t *testing.T) {
 func Test_Question1_Base64ToHex(t *testing.T) {
 	const in, out = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-	if x := HexEncodeToString(Base64DecodeString(in)); x != out {
+	if x := encoding.HexEncodeToString(encoding.Base64DecodeString(in)); x != out {
 		t.Errorf("HexEncodeToString(Base64DecodeString(%v)) = %v, want %v", in, x, out)
 	}
 }
@@ -30,9 +30,9 @@ func Test_Question1_Base64ToHex(t *testing.T) {
 
 func Test_Question2_fixedXor(t *testing.T) {
 	const in_a, in_b, out = "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965", "746865206b696420646f6e277420706c6179"
-	x := FixedXor(HexDecodeString(in_a), HexDecodeString(in_b))
+	x := FixedXor(encoding.HexDecodeString(in_a), encoding.HexDecodeString(in_b))
 
-	if HexEncodeToString(x) != out {
+	if encoding.HexEncodeToString(x) != out {
 		t.Errorf("FixedXor(%v, %v) = %v, want %v", in_a, in_b, x, out)
 	}
 }
@@ -42,7 +42,7 @@ func Test_Question2_fixedXor(t *testing.T) {
 func Test_Question3_DecryptSingleXor(t *testing.T) {
 	const in, out_result, out_key = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", "Cooking MC's like a pound of bacon", byte(88)
 
-	result, key, _ := DecryptSingleXor(HexDecodeString(in))
+	result, key, _ := DecryptSingleXor(encoding.HexDecodeString(in))
 
 	if string(result) != out_result || key != out_key {
 		t.Errorf("DecryptSingleXor = %#v, %#v want %#v, %#v", string(result), key, out_result, out_key)
@@ -58,7 +58,7 @@ func Test_Question4_DetectSingleXorLine(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(fs)
 
-	result, _, _ := DetectSingleXorLine(string(data), HexDecodeString)
+	result, _, _ := DetectSingleXorLine(string(data), encoding.HexDecodeString)
 
 	if string(result) != out {
 		t.Errorf("DetectXorLine(%#v) = %#v want %#v", in, string(result), out)
@@ -72,8 +72,8 @@ func Test_Question5_RepeatingXor(t *testing.T) {
 
 	x := RepeatXor([]byte(key), []byte(in))
 
-	if HexEncodeToString(x) != out {
-		t.Errorf("RepeatXor(%v, %v) = %v want %v", key, strconv.Quote(in), strconv.Quote(HexEncodeToString(x)), out)
+	if encoding.HexEncodeToString(x) != out {
+		t.Errorf("RepeatXor(%#v, %#v) = %#v want %#v", key, in, encoding.HexEncodeToString(x), out)
 	}
 }
 
@@ -84,7 +84,7 @@ func Test_Question6_hammingDistance(t *testing.T) {
 	x := hammingDistance([]byte(a), []byte(b))
 
 	if x != out {
-		t.Errorf("hammingDistance('%v', '%v') = %v want %v", a, b, x, out)
+		t.Errorf("hammingDistance(%#v, %#v) = %#v want %#v", a, b, x, out)
 	}
 }
 
@@ -94,7 +94,7 @@ func Test_Question6_DecryptXor(t *testing.T) {
 	defer fs.Close()
 
 	data, _ := ioutil.ReadAll(fs)
-	data = Base64DecodeString(string(data))
+	data = encoding.Base64DecodeString(string(data))
 
 	_, key := DecryptXor(data, 0.05)
 
@@ -110,7 +110,7 @@ func Test_Question7_DecryptAesEcb(t *testing.T) {
 	defer fs.Close()
 
 	data, _ := ioutil.ReadAll(fs)
-	data = Base64DecodeString(string(data))
+	data = encoding.Base64DecodeString(string(data))
 
 	result := DecryptAes(data, []byte("YELLOW SUBMARINE"))
 
@@ -126,7 +126,7 @@ func Test_Question8_DetectAesEcbLine(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(fs)
 
-	line, _, _ := DetectAesEcbLine(string(data), Base64DecodeString)
+	line, _, _ := DetectAesEcbLine(string(data), encoding.Base64DecodeString)
 
 	if line != out {
 		t.Errorf("DetectAesEcbLine(%#v) = %#v want %#v", in, line, out)
